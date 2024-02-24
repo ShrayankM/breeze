e6d88499545b4c5f7be3d7ca189a0289adf8b29e
ShrayankM <shraymist@gmail.com>
2024-02-24 07:37:05 +0530

    created new directory structure for new book application (#breeze)

---
+++ breeze/src/main/java/com/breeze/Breeze.java
@@ -0,0 +1,13 @@
+package com.breeze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Breeze {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Breeze.class, args);
+	}
+
+}
