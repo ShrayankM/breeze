04ba9a9bd09167e649cd9e2db210f9c28e3a037b
ShrayankM <shraymist@gmail.com>
2025-01-06 13:36:32 +0530

    updated config & backend google api for deployment

--- breeze/src/main/resources/sql/20240907-alter-tables.sql
+++ breeze/src/main/resources/sql/20240907-alter-tables.sql
@@ -2,3 +2,6 @@ ALTER TABLE `breeze_book_details` ADD COLUMN subtitle VARCHAR(255) DEFAULT NULL;
 ALTER TABLE `breeze_book_details` MODIFY COLUMN description TEXT;
 ALTER TABLE `breeze_book_details` ADD COLUMN language VARCHAR(16) DEFAULT NULL;
 ALTER TABLE breeze_user_book MODIFY COLUMN book_status ENUM('ADDED', 'READING', 'LIBRARY', 'COMPLETED', 'WISHLIST' );
+
+UPDATE UPDATE breeze_config  SET value = 'new-key' WHERE name = 'GOOGLE_BOOKS_API_HEADER_VALUE';
+UPDATE breeze_config  SET value = 'https://content-books.googleapis.com/books/v1/volumes' WHERE name = 'GOOGLE_BOOKS_API_URL';
